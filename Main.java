98de9ed13fa19af7431fa350baf5b69d738f209c
Alexander Ivanov <alexandrivanov7070@gmail.com>
2024-11-21 12:16:06 +0300

    add comments

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 class Main {
     public static void main(String[] args) {
+        // Вывод приветственного сообщения
         System.out.println(Utility.getWelcomeMessage());
         additionalMessage();
     }
 
+    // Вывод дополнительного сообщения
     public static void additionalMessage() {
         System.out.println("Дополнительное сообщение.");
     }
