75bf8ca3d97542aa2cfc1c339280346f1daae882
Alexander Ivanov <alexandrivanov7070@gmail.com>
2024-11-21 12:14:01 +0300

    add additionalMessage in Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
+        additionalMessage();
+    }
+
+    public static void additionalMessage() {
+        System.out.println("Дополнительное сообщение.");
     }
 }
